6628901fb55bd9f77b8c549d1496d564f005d759
andersonli <893093138@qq.com>
2017-08-11 17:21:20 +0800

    lhn 11/8 posji

---
+++ posji/posji.js
@@ -0,0 +1,169 @@
+'use strict'
+var inputs=[//创建inputs数组
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000003-2',
+    'ITEM000005',
+    'ITEM000005',
+    'ITEM000005'];
+
+window.onload=function () 
+{
+      var biaozhun=biaozhunhua(inputs);
+      var string=shuchu(biaozhun);
+      console.log(string);//控制台输出
+      function printTime()
+    {//打印现在时刻的函数
+           var dateDigitToString = function (num){
+              return num < 10 ? '0' + num : num;
+           };//第十位置加0凑成两位数，
+           var Rightnow=new Date(); //创建时间对象；
+           var year=dateDigitToString(Rightnow.getFullYear());   //获取系统的年；
+           var Month=dateDigitToString(Rightnow.getMonth()+1);   //获取系统月份，由于月份是从0开始计算，所以要加1
+           var day=dateDigitToString(Rightnow.getDate()); // 获取系统日，
+           var hours=dateDigitToString(Rightnow.getHours()); //获取系统时，
+           var minutes=dateDigitToString(Rightnow.getMinutes()); //分
+           var seconds=dateDigitToString(Rightnow.getSeconds()); //秒a
+           var string=year.toString()+'年'+Month+'月'+day.toString()+'日 '+hours.toString()+':'+minutes.toString()+':'+seconds.toString()+'\n----------------------\n';
+           return string;
+      }
+      //将inputs转换成每个对象元素具有count，discount属性的标准对象
+      function biaozhunhua(inputs)//把输入数据标准化的函数
+    {
+         var item=loadAllItems();//仓库商品单
+         var discountList=[];//打折商品
+         var discount=0;//打折数
+         var arr=[];//存放分割后的barcodes数组
+         //得到折扣的列表
+         for(var i=0,length=loadPromotions().length;i<length;++i)
+         {
+            if(loadPromotions()[i].type=='BUY_TWO_GET_ONE_FREE')
+            {
+                discountList=loadPromotions()[i].barcodes;
+            }
+         }
+          
+         //给所有商品增加count属性
+         for(i=0;i<item.length;i++)
+         {//循环遍历清单
+            item[i].count=0;//数量的初始值
+            for(var j=0;j<inputs.length;j++)
+            {//循环遍历输入数据
+                if(inputs[j].indexOf('-')!=-1)
+                {//如果有 -
+                    arr=inputs[j].split('-');//从 - 分隔成两个数组
+                    if(arr[0]==item[i].barcode)
+                    {//判断此barcodes是否存在
+                       item[i].count=parseInt(arr[1]);//存在则数量为 - 后的数据
+                     }
+                }
+                
+                else if(item[i].barcode==inputs[j])
+                {//是正常数据
+                    item[i].count++;//则增加一个
+                }
+            }
+         }
+         for(i=0;i<item.length;i++)
+         {//循环遍历清单
+            item[i].discount=0;//初值为0
+            for(j=0;j<discountList.length;j++)
+            {
+                if(item[i].barcode==discountList[j])
+                {
+                    discount=Math.floor(item[i].count/3);//打折数量
+                    if(discount>=1)
+                    {
+                    item[i].discount=discount;
+                    }
+                }
+            }
+         }
+         return item;
+      }
+      function shuchu(array)
+    {//输出函数
+          var string=printTime();
+          var sum=0;
+          var save=0;
+          for(var i=0;i<array.length;i++)
+          {//循环输出string
+              if(array[i].count!=0)
+              {
+                       string+='名称:'+array[i].name+',数量:'+array[i].count+array[i].unit+',单价:'+array[i].price.toFixed(2)+'(元),小计:'+(array[i].price*(array[i].count-array[i].discount)).toFixed(2)+'(元)\n';
+                       sum+=array[i].price*array[i].count;
+              }
+          }
+          
+          string+='----------------------\n挥泪赠送商品:\n';
+          for(i=0;i<array.length;i++)
+          {
+              if(array[i].discount!=0)
+              {
+                     string+='名称:'+array[i].name+',数量:'+array[i].discount+array[i].unit+'\n'
+                     save+=array[i].price*array[i].discount;
+              }
+          }
+          
+          sum=sum-save;
+          string+='----------------------\n总计:'+sum.toFixed(2)+'(元)\n节省:'+save.toFixed(2)+'(元)\n**********************';
+          return string;
+      }
+
+  }
+  function loadAllItems() {
+      return [
+          {
+              barcode: 'ITEM000000',
+              name: '可口可乐',
+              unit: '瓶',
+              price: 3.00
+          },
+          {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00
+          },
+          {
+              barcode: 'ITEM000002',
+              name: '苹果',
+              unit: '斤',
+              price: 5.50
+          },
+          {
+              barcode: 'ITEM000003',
+              name: '荔枝',
+              unit: '斤',
+              price: 15.00
+          },
+          {
+              barcode: 'ITEM000004',
+              name: '电池',
+              unit: '个',
+              price: 2.00
+          },
+          {
+              barcode: 'ITEM000005',
+              name: '方便面',
+              unit: '袋',
+              price: 4.50
+          }
+      ];
+  }
+
+  function loadPromotions() {
+      return [
+          {
+              type: 'BUY_TWO_GET_ONE_FREE',
+              barcodes: [
+                  'ITEM000000',
+                  'ITEM000001',
+                  'ITEM000005'
+              ]
+          }
+      ];
+  }
